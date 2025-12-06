dd7a3f743d9beb7c84b0d75af290a9d7e03d51b5
Nihad <mncnihad@gmail.com>
2025-12-06 12:30:16 +0530

    Created Footer page UI

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import AboutSection from "./pages/About";
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <AboutSection />
       <ConferenceThemes />
+      <Footer />
     </>
   );
 }
---
+++ Client/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="w-full bg-black text-white py-8 px-6 text-center">
+      <p className="text-[13px] leading-relaxed">
+        This is licensed under © Industrial Design Centre, Indian Institute of
+        Technology Bombay & Jain Deemed-to-be University, Kochi.
+      </p>
+
+      <a
+        href="#"
+        className="text-[13px] text-blue-400 underline mt-2 inline-block"
+      >
+        https://www.typoday.in
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
